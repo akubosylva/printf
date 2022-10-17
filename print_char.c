6f30317b50a936f1bdba6614c3ec9372e4c03b97
akubosylva <akubosilver@gmail.com>
2022-10-17 12:46:52 +0100

    Modify String function

--- print_char.c
+++ print_char.c
@@ -15,3 +15,22 @@ int print_char(va_list list, int length)
 	length++;
 	return (length);
 }
+
+/**
+ * print_str - prints input as string to stdout
+ * @list: argument list
+ * @length: length of input
+ * Return: length
+ */
+
+int print_str(va_list list, int length)
+{
+	int i;
+	const char *str = va_arg(list, const char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++, length++)
+		_putchar(str[i]);
+	return (length);
+}
