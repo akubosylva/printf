96162c9cb9c345a5ee79df0443280a40d6c241ba
akubosylva <akubosilver@gmail.com>
2022-10-17 12:31:48 +0100

    Get Function ptr

---
+++ get_func.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  *get_func - returns pointer to function.
+  *@c: character to be used to find pointer function.
+  *Return: pointer to function or NULL.
+  */
+
+int (*get_func(char c))(va_list, int)
+{
+	func funcArr[] = {
+		{'c', print_char},
+		{'s', print_str},
+		{'d', print_int},
+		{'i', print_int},
+		{'u', print_unsig},
+		{'o', print_octal},
+		{'x', print_hex},
+		{'X', print_Hex},
+		{'R', print_rot13},
+		{'b', print_b},
+		{'S', print_Str},
+		{'\0', NULL}
+	};
+
+	int i = 0;
+
+	while (funcArr[i].placeholder != '\0')
+	{
+		if (funcArr[i].placeholder == c)
+			return (funcArr[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
