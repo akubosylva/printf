a591cd7e9a732a478795a334aa871354bcfdb759
ApehEndurance <apehendurance133@gmail.com>
2022-10-17 19:19:02 -0700

    Functions

---
+++ functions.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	    char color[20];
+	        char pluralnoun[20];
+		    char celebrity[20];
+
+		        prinf("Eneter a color:");
+			    scanf("%s", color);
+			        prinf("Eneter a plural noun:");
+				    scanf("%s", pluralnoun);
+				        prinf("Eneter a celebrity:");
+					    scanf("%s", celebrity);
+
+					        printf("Roses are %s\n", color);
+						    printf("%s are blue\n", pluralnoun);
+						        printf("I love %s\n", celebrity);
+
+							    return 0;
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	    char color[20];
+	        char pluralnoun[20];
+		    char celebrity[20];
+
+		        prinf("Eneter a color:");
+			    scanf("%s", color);
+			        prinf("Eneter a plural noun:");
+				    scanf("%s", pluralnoun);
+				        prinf("Eneter a celebrity:");
+					    scanf("%s", celebrity);
+
+					        printf("Roses are %s\n", color);
+						    printf("%s are blue\n", pluralnoun);
+						        printf("I love %s\n", celebrity);
+
+							    return 0;
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	    char color[20];
+	        char pluralnoun[20];
+		    char celebrity[20];
+
+		        prinf("Eneter a color:");
+			    scanf("%s", color);
+			        prinf("Eneter a plural noun:");
+				    scanf("%s", pluralnoun);
+				        prinf("Eneter a celebrity:");
+					    scanf("%s", celebrity);
+
+					        printf("Roses are %s\n", color);
+						    printf("%s are blue\n", pluralnoun);
+						        printf("I love %s\n", celebrity);
+
+							    return 0;
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	    char color[20];
+	        char pluralnoun[20];
+		    char celebrity[20];
+
+		        prinf("Eneter a color:");
+			    scanf("%s", color);
+			        prinf("Eneter a plural noun:");
+				    scanf("%s", pluralnoun);
+				        prinf("Eneter a celebrity:");
+					    scanf("%s", celebrity);
+
+					        printf("Roses are %s\n", color);
+						    printf("%s are blue\n", pluralnoun);
+						        printf("I love %s\n", celebrity);
+
+							    return 0;
+}
+
