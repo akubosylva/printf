5b6187097ffd3eeb9cd28664ba27c02ea6aab9bc
akubosylva <akubosilver@gmail.com>
2022-10-19 12:32:20 +0100

    Print pointer

---
+++ print_ptr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_ptr - print address of value
+ * @list: argument list
+ * @length: length
+ * Return: length
+ */
+
+int print_ptr(va_list list, int length)
+{
+	int i;
+	unsigned long address;
+	void *ptr = va_arg(list, void *);
+
+	if (ptr == NULL)
+	{
+		length += _putchar('(');
+		length += _putchar('n');
+		length += _putchar('i');
+		length += _putchar('l');
+		length += _putchar(')');
+		return (length);
+	}
+
+	address = (unsigned long)ptr;
+		length += _putchar('0');
+		length += _putchar('x');
+
+	for (i = 0; i < 1; i++)
+		length += print_numbers(address, 16, "0123456789abcdef");
+	return (length);
+}
