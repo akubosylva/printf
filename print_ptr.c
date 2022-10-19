afb12fe9f3a0b97252caf205de53c9fdf1cd86ed
akubosylva <akubosilver@gmail.com>
2022-10-19 18:19:45 +0100

    Print pointer

--- print_ptr.c
+++ print_ptr.c
@@ -1,50 +1,31 @@
 #include "main.h"
+
 /**
  * print_ptr - print address of value
  * @list: argument list
  * @length: length
  * Return: length
  */
+
 int print_ptr(va_list list, int length)
 {
-	char *str;
-	unsigned long int dec, buffr;
-	char c[100];
-	int count, n, i = 0, j;
+	int i;
+	char *str = "(nil)";
+	unsigned long address;
+	void *ptr = va_arg(list, void *);
 
-	dec = (unsigned long int)va_arg(list, void*);
-	str = "(nil)";
-
-	buffr = dec;
-	count = 1;
-	if (!dec)
+	if (ptr == NULL)
 	{
-		for (i = 0; str[i] != '\0'; i++, length++)
+		for (i = 0; str[i]; i++, length++)
 			_putchar(str[i]);
+
 		return (length);
 	}
-	while (buffr)
-	{
-		buffr /= 16;
-		count++;
-	}
-	c[count + 1] = '\0';
-	while (dec > 0)
-	{
-		n = (dec % 16);
-		if (n >= 0 && n <= 9)
-			c[count] = ((char)(n + '0'));
-		else
-			c[count] = ((char)(n + 'W'));
-		count--;
-		dec /= 16;
-	}
-	c[0] = '0';
-	c[1] = 'x';
-	while (c[i] != '\0')
-	{
-		_putchar(c[i]);
-		i++, length++;
-	}
+
+	address = (unsigned long)ptr;
+	length += _putchar('0');
+	length += _putchar('x');
+	length += print_numbers(address, 16, "0123456789abcdef");
+
 	return (length);
 }
