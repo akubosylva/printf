9a43a2205f807cabcaac546f1888e049a9e96932
akubosylva <akubosilver@gmail.com>
2022-10-19 13:00:41 +0100

    Pointer

--- print_ptr.c
+++ print_ptr.c
@@ -10,24 +10,24 @@
 int print_ptr(va_list list, int length)
 {
 	int i;
+	char *str = "(null)";
 	unsigned long address;
 	void *ptr = va_arg(list, void *);
 
 	if (ptr == NULL)
 	{
-		_putchar('(');
-		_putchar('n');
-		_putchar('i');
-		_putchar('l');
-		_putchar(')');
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			length++;
+			_putchar(str[i]);
+		}
 		return (length);
 	}
 
 	address = (unsigned long)ptr;
 		length += _putchar('0');
 		length += _putchar('x');
-
-	for (i = 0; i < 1; i++)
 		length += print_numbers(address, 16, "0123456789abcdef");
+
 	return (length);
 }
