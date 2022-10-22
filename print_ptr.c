c1cdd8c3a7e288e4c6f1fb6fb98aa9f3f3775d28
akubosylva <akubosilver@gmail.com>
2022-10-22 21:54:20 +0100

    Print Ptr

--- print_ptr.c
+++ print_ptr.c
@@ -1,4 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * printf_hex_aux - prints an hexgecimal number.
+ * @num: number to print.
+ * Return: counter.
+ */
+
+int printf_hex_aux(unsigned long int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned long int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(long int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
 
 /**
  * print_ptr - print address of value
@@ -9,23 +46,21 @@
 
 int print_ptr(va_list list, int length)
 {
+	char *str = "(nil)";
 	int i;
 	unsigned long int address;
-	char *str = "(nil)";
 	void *ptr = va_arg(list, void *);
 
 	if (ptr == NULL)
 	{
-		for (i = 0; str[i]; i++, length++)
+		for (i = 0; str[i] != '\0'; i++)
 			_putchar(str[i]);
-
-		return (length);
+		return (i);
 	}
 
 	address = (unsigned long int)ptr;
-	length += _putchar('0');
-	length += _putchar('x');
-	length += print_numbers(address, 16, "0123456789abcdef");
-
-	return (length);
+	_putchar('0');
+	_putchar('x');
+	length = printf_hex_aux(address);
+	return (length + 2);
 }
